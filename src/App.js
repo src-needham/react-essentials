ce6497da24eeddc6fe2e88646705cc20a5c0ffd8
Sam Needham <src.needham@gmail.com>
2021-07-15 14:51:35 +0100

    Adds App test

--- src/App.js
+++ src/App.js
@@ -3,30 +3,11 @@ import './App.css';
 
 // https://api.github.com/users/src-needham
 
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
+function App() {
 
   return (
     <div>
-      <h1>{data.name}</h1>
-      <p>{data.bio}</p>
-      <img alt={data.login} src={data.avatar_url} />
+      <h1>Hello React Testing Library</h1>
     </div>
   );
 }
