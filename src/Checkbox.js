43a7079c7de4fc4d66e22c22fc37b48de785e725
Sam Needham <src.needham@gmail.com>
2021-07-16 10:58:32 +0100

    Completes Checkbox and test

---
+++ src/Checkbox.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from "react";
+
+export function Checkbox() {
+  const [checked, toggle] = useReducer(
+    checked => !checked, 
+    false
+  );
+
+  return(
+    <>
+    <label htmlFor="checkbox">
+      {checked ? "checked" : "unchecked"}
+    </label>
+    <input 
+      type="checkbox" 
+      value={checked} 
+      onChange={toggle}
+    />
+    </>
+  );
+}
