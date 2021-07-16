b7394e2360e457143b110f571467d3f022586e52
Sam Needham <src.needham@gmail.com>
2021-07-16 14:01:47 +0100

    Adds paths to App and index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Events, Contact } from "./pages";
 
 // https://api.github.com/users/src-needham
 
@@ -7,7 +9,12 @@ function App() {
 
   return (
     <div>
-      <h1>Hello React Testing Library</h1>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/events" element={<Events />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </div>
   );
 }
